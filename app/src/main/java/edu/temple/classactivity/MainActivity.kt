8e21b3a926d6426604086b6ef2459c16b306c787
Nathan Marzina <46549002+NJMarzina@users.noreply.github.com>
2025-01-28 12:13:45 -0500

    Updated button with OnClick function which updates button to say "Button clicked"

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,31 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickMeButton = findViewById(R.id.button)
+
+        clickMeButton.setOnClickListener {
+            clickMeButton.text = "Button clicked"
+        }
+
+        //val ocl = object: OnClickListener {
+        //    override fun onClick(p0: View?) {
+        //        (p0 as Button).text = "Button clicked"
+        //    }
+        //}
     }
 }
